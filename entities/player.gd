938cd10bf3f4a10024f188f0197763700e183d22
citrus <curtis.c.king@gmail.com>
2025-02-21 00:39:07 -0500

    player animation

--- entities/player.gd
+++ entities/player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var speed : int
+var is_moving : bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,6 +19,19 @@ func move(delta):
     position.x += Input.get_axis("left", "right" ) * speed * delta
     position.y += Input.get_axis("up", "down") * speed * delta
     
+    # i feel like this can be shortened, but idk how to do it
+    if (Input.is_action_pressed("left") or 
+    Input.is_action_pressed("right") or 
+    Input.is_action_pressed("down") or 
+    Input.is_action_pressed("up")):
+        is_moving = true;
+    else:
+        is_moving = false;
+    
+    if (is_moving):
+        $AnimatedSprite2D.play("walk")
+    else:
+        $AnimatedSprite2D.stop()
     
 func look_at_mouse():
     look_at(get_global_mouse_position())
