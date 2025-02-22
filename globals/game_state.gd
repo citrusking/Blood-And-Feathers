a55ba7cc857c6106a1703ab94262464079cd468e
gman02468 <146103033+gman02468@users.noreply.github.com>
2025-02-22 16:25:10 -0500

    Chickens & different Enemy Spawning
     - Added Chicken enemy type
     - Changed some collision methods to work with chicken enemy type
     - Changed Enemy Spawning to take from an array instead of a single enemy

--- globals/game_state.gd
+++ globals/game_state.gd
@@ -7,9 +7,9 @@ var night : int # this is the level, or wave, or whatever
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
