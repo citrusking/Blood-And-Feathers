d259645f6eb2048e944d1c311a290724c14f9f77
citrus <curtis.c.king@gmail.com>
2025-02-22 20:47:30 -0500

    small bug fix
    didn't drag pelican to packed scene var

--- entities/other/feather/feather.gd
+++ entities/other/feather/feather.gd
@@ -3,14 +3,14 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play("flymetothemoon")
+    play("flymetothemoon")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 func _on_area_2d_body_entered(body):
-	GameState.feathers = GameState.feathers + 1
-	queue_free()
+    GameState.feathers = GameState.feathers + 1
+    queue_free()
